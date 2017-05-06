d69455243a3fbe032655a65c5da7b52864402eed
talbaumel <talbaumel@gmail.com>
2017-05-06 14:19:41 +0300

    py3/dynet1

--- softmax/data.py
+++ softmax/data.py
@@ -11,22 +11,22 @@ VOCAB_SIZE = len(characters)
 
 def sample_model(min_length, max_lenth):
     random_length = randrange(min_length, max_lenth)                             # Pick a random length
-    random_char_list = [choice(characters[:-1]) for _ in xrange(random_length)]  # Pick random chars
+    random_char_list = [choice(characters[:-1]) for _ in range(random_length)]  # Pick random chars
     random_string = ' '.join(random_char_list)
     reverse_string = ' '.join(random_char_list[::-1])
     return random_string, reverse_string  # Return the random string and its reverse
 
 MAX_STRING_LEN = 5
 
-train_set = [sample_model(1, MAX_STRING_LEN) for _ in xrange(3000)]
-val_set = [sample_model(1, MAX_STRING_LEN) for _ in xrange(50)]
+train_set = [sample_model(1, MAX_STRING_LEN) for _ in range(3000)]
+val_set = [sample_model(1, MAX_STRING_LEN) for _ in range(50)]
 
 def set_vocab_size(size):
     global characters, char2int, int2char, VOCAB_SIZE
     characters = [str(i) for i in range(size)]
     characters.append(EOS)
-
+    
     int2char = list(characters)
     char2int = {c:i for i,c in enumerate(characters)}
-
+    
     VOCAB_SIZE = len(characters)
